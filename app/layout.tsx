76517d1263a07b2c2f701e0b8478816434a9b5c0
Santosh Patel <santoshpatelvns5@gmail.com>
2025-11-26 00:12:44 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theam-provider";
 
 const geistSans = Geist({
@@ -12,6 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
